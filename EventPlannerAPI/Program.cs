c3c2162238551447ec4af7d846187d040ce6e05a
x00167885 <x00167885@mytudublin.ie>
2024-04-05 02:33:55 +0100

    Small modification

--- EventPlannerAPI/Program.cs
+++ EventPlannerAPI/Program.cs
@@ -17,7 +17,7 @@ namespace EventPlannerAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || true)
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
